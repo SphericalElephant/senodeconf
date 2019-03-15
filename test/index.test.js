04c3a9e534bde563bad93e039beb3bb7dfa2b72c
Patrick Sturm <p.sturm@sphericalelephant.com>
2019-03-15 14:48:01 +0100

    fixed linter issues

--- test/index.test.js
+++ test/index.test.js
@@ -92,17 +92,17 @@ describe('senodeconf', () => {
   it('should use the default env separator if no other has been specified', done => {
     const opts = {
 
-    }
+    };
     const oldNconf = _senodeconf.__get__('nconf');
     const nconfSpy = {
       argv: sinon.stub().returnsThis(),
       env: sinon.stub().returnsThis(),
       file: sinon.stub().returnsThis()
-    }
+    };
 
     _senodeconf.__set__('nconf', nconfSpy);
     obtainConfigObject(...['global', 'user', 'local'].map(type => getConfigPath('local', 'name', 'development', opts)), opts);
-    expect(nconfSpy.env.calledWith({separator: '_'})).to.be.true
+    expect(nconfSpy.env.calledWith({separator: '_'})).to.be.true;
     _senodeconf.__set__('nconf', oldNconf);
 
     done();
@@ -111,17 +111,17 @@ describe('senodeconf', () => {
   it('should use the specified env separator', done => {
     const opts = {
       envSeparator: '___'
-    }
+    };
     const oldNconf = _senodeconf.__get__('nconf');
     const nconfSpy = {
       argv: sinon.stub().returnsThis(),
       env: sinon.stub().returnsThis(),
       file: sinon.stub().returnsThis()
-    }
+    };
 
     _senodeconf.__set__('nconf', nconfSpy);
     obtainConfigObject(...['global', 'user', 'local'].map(type => getConfigPath('local', 'name', 'development', opts)), opts);
-    expect(nconfSpy.env.calledWith({separator: '___'})).to.be.true
+    expect(nconfSpy.env.calledWith({separator: '___'})).to.be.true;
     _senodeconf.__set__('nconf', oldNconf);
 
     done();
